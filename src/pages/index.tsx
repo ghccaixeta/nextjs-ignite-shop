3862b8f03bba785b706f226c6ce2f2ce652ebd9e
Gustavo Caixeta <gustavo.caixeta@nanoincub.com.br>
2024-05-03 09:05:52 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import imgage1 from '../assets/camisetas/1.png'
+import imgage2 from '../assets/camisetas/2.png'
+import imgage3 from '../assets/camisetas/3.png'
 
 
 const Button = styled('button',{
@@ -11,8 +16,43 @@ const Button = styled('button',{
 
 export default function Home() {
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image
+          src={imgage1}
+          width={520}
+          height={480}
+          alt=""
+         />
+         <footer>
+          Camiseta X
+          <span>R$ 79,90</span>
+         </footer>
+      </Product>
+      <Product>
+        <Image
+          src={imgage2}
+          width={520}
+          height={480}
+          alt=""
+         />
+         <footer>
+          Camiseta X
+          <span>R$ 79,90</span>
+         </footer>
+      </Product>
+      <Product>
+        <Image
+          src={imgage3}
+          width={520}
+          height={480}
+          alt=""
+         />
+         <footer>
+          Camiseta X
+          <span>R$ 79,90</span>
+         </footer>
+      </Product>
+    </HomeContainer>
   );
 }
